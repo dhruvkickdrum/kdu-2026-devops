49d2b300ed62d0bec5960509da7f9e2b0c36625b
Dhruv-Bansal7 <dhruvbansal7727@gmail.com>
2026-02-24 19:44:37 +0530

    changes

--- terraform/backend/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "dhruv-tf-state-bucket"
-    key            = "assignment-2/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "dhruv-terraform-locks"
-    encrypt        = true
-  }
-}
