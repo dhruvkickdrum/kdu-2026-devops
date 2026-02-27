b63130286fe4aea87ba69584a0307a9a4c80b892
dhruvKickdrum <dhruv.bansal@kickdrumtech.com>
2026-02-27 18:22:52 +0530

    changes

--- cicdapp/src/main/java/com/example/cicdapp/CicdappApplication.java
+++ cicdapp/src/main/java/com/example/cicdapp/CicdappApplication.java
@@ -2,9 +2,16 @@ package com.example.cicdapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicdappApplication {
+public class CicdappApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CicdappApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdappApplication.class, args);
